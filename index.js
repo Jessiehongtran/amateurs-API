4f6a7d16fb5409e868375de59b6e65702932bd29
Your Name <you@example.com>
2020-09-08 14:33:50 -0700

    tried changing server to app

--- index.js
+++ index.js
@@ -29,13 +29,12 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true})); 
 app.use('/events', eventRoutes);
 app.use('/users', userRoutes)
 app.use('/', (req,res) => {
     res.send('helloooo')
 })
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
 
 
 
